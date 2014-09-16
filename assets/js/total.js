403b258a4aeb210946c6484a68cbfb89bd13d646
wushuyi <wushuyi541@gmail.com>
2014-09-16 18:09:45 +0800

    mz

--- assets/js/total.js
+++ assets/js/total.js
@@ -1,11 +1,22 @@
-﻿$(document).ready(function(){
-	window.totalE = window.totalE || {};
-	var $total = $(window.totalE);
-	$total.on('total', function(e, type, name){
-		console.log('ok');
-		mz[type](name);
-	});
-	$('.cruze_logo').on('click', function(e){
-		
-	});
-});
+﻿;(function ($) {
+    $(document).ready(function(){
+        var total = {};
+        total.pgname = 'PC端-科鲁兹官网';
+        mz.pv(total.pgname + '-首页');
+        $('#main .cruze_logo').on('click.total', function(e){
+            mz.event(total.pgname + '-科鲁兹logo');
+        });
+        $('#main .chevrolet_logo').on('click.total', function(e){
+            mz.event(total.pgname + '-雪佛兰logo');
+        });
+        $('#main .car1 .gotoBtn').on('click.total', function(e){
+            mz.event(total.pgname + '-科鲁兹经典');
+        });
+        $('#main .car2 .gotoBtn').on('click.total', function(e){
+            mz.event(total.pgname + '-全新科鲁兹');
+        });
+        $('#main .car3 .gotoBtn').on('click.total', function(e){
+            mz.event(total.pgname + '-科鲁兹掀背车');
+        });
+    });
+})(window.jQuery);
